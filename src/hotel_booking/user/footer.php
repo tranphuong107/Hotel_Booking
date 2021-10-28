387f1e3a02c3cf6a431c4e3ab4bf002ce011a958
TraMyyyy <ttmy2061@gmail.com>
2021-10-28 23:04:03 +0700

    change footer and header

--- src/hotel_booking/user/footer.php
+++ src/hotel_booking/user/footer.php
@@ -1,17 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-  </head>
-  <body>
-
-    <!-- Footer -->
+<!-- footer -->
 <footer class="text-center text-lg-start bg-dark text-muted">
 
     <!-- Section: Links  -->
--- src/hotel_booking/user/index.php
+++ src/hotel_booking/user/index.php
@@ -1,19 +1,29 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Trang chủ</title>
-  </head>
-  <body>
         <?php
             include ('header.php');
         ?>
+
+    <!-- Banner -->
+   <div style = "">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-interval = "1000">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../images/b.jpg" class="d-block w-100 img-fluid" alt="">
+            </div>
+            <div class="carousel-item active">
+                <img src="../images/banner1.jpg" class="d-block w-100 img-fluid" alt="">
+            </div>
+            <div class="carousel-item">
+                <img src="../images/banner2.jpg" class="d-block w-100 " alt="">
+            </div>
+            <div class="carousel-item">
+                <img src="../images/banner3.jpg" class="d-block w-100" alt="">
+            </div>
+        </div>
+    </div>
+   </div>
+    <!-- Banner end -->
+
     <!-- Service -->
     <section>
         <div class = "text-uppercase fw-bold mb-4" style = "color:#FFC40E">
@@ -39,16 +49,3 @@
         <?php
             include ('footer.php');
         ?>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
