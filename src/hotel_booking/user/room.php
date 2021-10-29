aa23df74d304bcc7ea2f9b74402acefaf01e5bb7
ladulee <doh9006@gmail.com>
2021-10-29 16:53:56 +0700

    add room.php và process_search.php

---
+++ src/hotel_booking/user/room.php
@@ -0,0 +1,33 @@
+<?php include('header.php'); ?>
+    <main class="">
+        <section> 
+            <div>
+                <!-- Start Search -->
+                <div class="container-fluid px-0">
+
+                    <div class=" Food-Search px-0 text-center">
+                        <form action="" class="">
+                        <input type="search"  class="my-2 p-2 border-0 rounded-2 " style="width: 400px;" name="Search" placeholder="Search for Room...">
+                        <input type="submit" name="submit" value="Search" class="py-2 btn btn-primary  my-color border-0">
+                        </form>
+                    </div>
+                </div>
+                <!-- End search -->
+            </div>
+            <div>
+                <div class="container">
+                    <div class="room">
+                            room 1
+                    </div>
+                    <div class="room">
+                            room 2
+                    </div>      
+                </div>
+            </div>
+
+        </section>
+    </main>
+
+    
+
+<?php include('footer.php'); ?>
