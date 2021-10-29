29752e581bfaadc934d5db42cdddc71f8c429355
TraMyyyy <90631268+TraMyyyy@users.noreply.github.com>
2021-10-29 10:11:28 +0700

    Tramy (#3)
    * done header
    
    * done footer
    
    * change footer and header
    
    * update homepage

--- src/hotel_booking/user/footer.php
+++ src/hotel_booking/user/footer.php
@@ -1,12 +1,143 @@
-   <!-- Optional JavaScript; choose one of the two! -->
+<!-- footer -->
+<footer class="text-center text-lg-start bg-dark text-muted">
+
+    <!-- Section: Links  -->
+    <section class="p-5">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <img src="http://mauweb.monamedia.net/howello/wp-content/uploads/2019/03/logo_footer.png" alt="" class = "img-fluid">
+            <p>
+            Đến với Khu nghỉ dưỡng Premier Village Mona Media, quý khách sẽ được trải nghiệm dịch vụ Spa cao cấp, trung tâm thể hình hiện đại và các hoạt động giải trí trên biển
+            </p>
+            <ul class = "row" style = "list-style: none">
+              <li class = "col-3">
+              <a href="">
+                <img src="../images/facebook.png" alt="#" class = "img-fluid" data-bs-toggle="tooltip" data-bs-placement="top" title="Follow on Facebook">
+              </a></li>
+              <li class = "col-3">
+              <a href="#">
+                <img src="../images/insta.png" alt="#" class = "img-fluid" data-bs-toggle="tooltip" data-bs-placement="top" title="Follow on Instagram">
+              </a></li>
+              <li class = "col-3">
+              <a href="#">
+                <img src="../images/twitter.png" alt="#" class = "img-fluid" data-bs-toggle="tooltip" data-bs-placement="top" title="Follow on Twitter">
+              </a></li>
+            </ul>
+          </div>
+          <!-- <div class = "row">
+            
+          </div> -->
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h5 class="text-uppercase fw-bold mb-4" style = "color:#FFC40E">
+              Dịch vụ
+            </h5>
+            <style>
+              .ser:hover{
+                color: #FFC40E !important;
+              }
+            </style>
+            <p>
+              <a href="#!" class="ser text-decoration-none" style = "color: white">Nhà hàng</a>
+            </p>
+            <p>
+              <a href="#!" class="ser text-decoration-none" style = "color: white">Khách sạn</a>
+            </p>
+            <p>
+              <a href="#!" class="ser text-decoration-none" style = "color: white">Phòng họp</a>
+            </p>
+            <p>
+              <a href="#!" class="ser text-decoration-none" style = "color: white">Đám cưới</a>
+            </p>
+            <p>
+              <a href="#!" class="ser text-decoration-none" style = "color: white">Massage</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h5 class="text-uppercase fw-bold mb-4" style = "color:#FFC40E">
+              Liên kết
+            </h5>
+            <p>
+              <a href="#!" class="ser text-decoration-none" style = "color: white">Trang chủ</a>
+            </p>
+            <p>
+              <a href="#!" class="ser text-decoration-none" style = "color: white">Dịch vụ</a>
+            </p>
+            <p>
+              <a href="#!" class="ser text-decoration-none" style = "color: white">Phòng</a>
+            </p>
+            <p>
+              <a href="#!" class="ser text-decoration-none" style = "color: white">Đơn hàng</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <style>
+              .col:hover{
+                opacity: 0.8;
+              }
+            </style>
+            <!-- Links -->
+            <h5 class="text-uppercase fw-bold mb-4" style = "color:#FFC40E">
+              Hình ảnh
+            </h5>
+            <!-- picture -->
+            <ul class = "row p-0 m-0 large-columns-2" style = "list-style:none">
+              <li class = "col p-0 m-1">
+              <a href="">
+                <img src="../images/double-room.jpg" alt="#" class = "img-fluid">
+              </a></li>
+              <li class = "col p-0 m-1">
+              <a href="#">
+                <img src="../images/single-room.jpg" alt="#" class = "img-fluid">
+              </a></li>
+            </ul>
+            <ul class = "row p-0 m-0 medium-columns-2" style = "list-style:none">
+             <li class = "col col-auto p-0 m-1">
+              <a href="#">
+                <img src="../images/image-index.jpg" alt="#" class = "img-fluid">
+              </a></li>
+            </ul>
+            </div>
+            
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2021 Copyright:
+      <a class="text-reset fw-bold" href="#">CSE485.CNW</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+
+    <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
   </body>
 </html>
--- src/hotel_booking/user/header.php
+++ src/hotel_booking/user/header.php
@@ -6,11 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+      .nav-link:hover{
+        color: #FFC40E !important;
+      }
+    </style>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <section style = "background-color:white">
+    <div class = "container" >
+    <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top pt-3">
+      <div class="container row align-items-center">
+        <div class = "col-3">
+          <a class="navbar-brand" href="index.php">
+            <img src="http://mauweb.monamedia.net/howello/wp-content/uploads/2019/03/logo.png" alt=""class = "img-fluid">
+          </a></div>
+        <div class = "col-auto">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav mx-4">
+                <a class="nav-link active fw-bold fs-6 text-decoration-none mx-2" style = "color:black" aria-current="page" href="index.php">Trang chủ</a>
+                <a href="room.php" class = "nav-link fs-6 fw-bold text-decoration-none  mx-2" style = "color:black">Phòng</a>
+                <a href="service.php" class = "nav-link  fs-6 fw-bold text-decoration-none mx-2" style = "color:black">Dịch vụ</a>
+                <a href="index.php" class = "nav-link fs-6 fw-bold text-decoration-none mx-2" style = "color:black">Đặt hàng</a>
+              </div>
+            </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</section>
 
  
