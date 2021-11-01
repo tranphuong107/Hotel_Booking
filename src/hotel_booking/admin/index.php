fb0bfec184ee4e1054b6399e69189beaa8effdcd
Your Name <you@example.com>
2021-11-01 22:33:10 +0700

    admin index

--- src/hotel_booking/admin/index.php
+++ src/hotel_booking/admin/index.php
@@ -1,8 +1,78 @@
 <?php
     include ('header.php');
+    include ('../config.php');
 ?>
-<div class="content">
+<div class="content p-4">
+    <div class="col-4 text-center  p-5 m-4 fw-bold float-start" style=" background-color:#f1f2f6;">
 
+        <?php 
+            //Sql Query 
+            $sql = "SELECT * FROM tb_rooms";
+            //Execute Query
+            $res = mysqli_query($conn, $sql);
+            //Count Rows
+            $count = mysqli_num_rows($res);
+        ?>
+
+    <h1><?php echo $count; ?></h1>
+    <br />
+    <label for="" class="fs-4">PHÒNG</label>
+    </div>
+
+    <div class="col-4 text-center p-5 m-4 fw-bold float-start " style=" background-color:#f1f2f6;">
+
+        <?php 
+            //Sql Query 
+            $sql = "SELECT * FROM tb_services";
+            //Execute Query
+            $res = mysqli_query($conn, $sql);
+            //Count Rows
+            $count = mysqli_num_rows($res);
+        ?>
+
+    <h1><?php echo $count; ?></h1>
+    <br />
+    <label for="" class="fs-4">DỊCH VỤ</label>
+    </div>
+
+    <div class="col-4 text-center p-5 m-4 fw-bold float-start" style=" background-color:#f1f2f6;">
+
+        <?php 
+            //Sql Query 
+            $sql = "SELECT * FROM tb_customers";
+            //Execute Query
+            $res = mysqli_query($conn, $sql);
+            //Count Rows
+            $count = mysqli_num_rows($res);
+        ?>
+
+    <h1><?php echo $count; ?></h1>
+    <br />
+    <label for="" class="fs-4">KHÁCH</label>
+    </div>
+    <div class="col-4 text-center p-5 m-4 fw-bold float-start" style=" background-color:#f1f2f6;">
+
+        <?php 
+            //Sql Query 
+            $sql = "SELECT * FROM tb_order_rooms";
+            //Execute Query
+            $res = mysqli_query($conn, $sql);
+            //Count Rows
+            $count = mysqli_num_rows($res);
+
+            $sql = "SELECT * FROM tb_order_services";
+            //Execute Query
+            $res = mysqli_query($conn, $sql);
+            //Count Rows
+            $count1 = mysqli_num_rows($res);
+            $tong = $count +$count1;
+
+        ?>
+
+    <h1><?php echo $tong; ?></h1>
+    <br />
+    <label for="" class="fs-4">ĐƠN HÀNG</label>
+    </div>
 </div>
 
 
