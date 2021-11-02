36efe8c646d0f401266c31e7168c2dddb1927439
ladulee <doh9006@gmail.com>
2021-11-02 10:12:30 +0700

    commit

---
+++ src/hotel_booking/user/details-room.php
@@ -0,0 +1,47 @@
+<?php include('header.php'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item ">
+                                <a href="index.php">
+                                    <span class="text-dark">TRANG CHỦ</span>
+                                </a></li>
+                            <li class="breadcrumb-item active ">
+                                <a href="room.php">
+                                    <span class="text-dark">PHÒNG</span>
+                                </a></li>
+                                <li class="breadcrumb-item active ">
+                                <a href="#">
+                                    <span class="text-dark">Chi tiết</span>
+                                </a></li>
+                        </ol>
+                    </nav>                  
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                        <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+            </div>
+
+        </div>
+    </div>
+
+
+<?php include('footer.php'); ?>
--- src/hotel_booking/user/process-search.php
+++ src/hotel_booking/user/process-search.php
@@ -46,33 +46,47 @@
             </div>
             
 	    </div>
-        <?php
-            $sql = "SELECT room_image,room_type, room_price FROM tb_rooms";
-            $result = mysqli_query($conn,$sql);
-
-            // Phân tích và xử lí kết quả
-            if(mysqli_num_rows($result) > 0){
-                echo'<div class="row row-cols-1 row-cols-md-3 g-5">';
-                while($row = mysqli_fetch_assoc($result)){
-                    
-                    echo '<div class="col">';
-                        echo'<div class="card">';
-                        echo ' <a href="details-room.php" class="card-img " >';
-                        echo'   <img src="../images/'.$row['room_image'].'" class="card-img-top" alt="...">';
-                        echo' </a>';
-                        echo'<div class="card-body">';
-                        echo'   <h5 class="card-title">'.$row['room_type'].'</h5>';
-                        echo'   <p class="card-text  ">Giá: '.$row['room_price'].'₫</p>';
-                        echo'  <a href="details-room.php" class="btn btn-outline-dark">Chi tiết>></a>';
-                        echo'</div>';
-                        echo'</div>';
-                    echo'</div>';
-               
+        
+        <?php 
+                if(isset($_POST['btnsearch'])){
+                    $search = $_POST['search'];
                 }
-                echo'</div>';
-            }
+               
+                $sql = "SELECT * FROM tb_rooms WHERE room_type LIKE '%$search%' ";
+              
+                //Execute the Query
+                $result = mysqli_query($conn, $sql);
 
-        ?> 
+           
+                if(mysqli_num_rows($result) > 0){
+                    echo'<div class="row row-cols-1 row-cols-md-3 g-5 ">';
+                    while($row = mysqli_fetch_assoc($result)){
+                        
+                        echo '<div class="col">';
+                            echo'<div class="card">';
+                            echo ' <a href="details-room.php" class="card-img " >';
+                            echo'   <img src="../images/'.$row['room_image'].'" class="card-img-top" alt="...">';
+                            echo' </a>';
+                            echo'<div class="card-body">';
+                            echo'   <h5 class="card-title">'.$row['room_type'].'</h5>';
+                            echo'   <p class="card-text  ">Giá: '.$row['room_price'].'₫</p>';
+                            echo'  <a href="details-room.php" class="btn btn-outline-dark">Chi tiết>></a>';
+                            echo'</div>';
+                            echo'</div>';
+                        echo'</div>';
+                   
+                    }
+                    echo'</div>';
+                }else
+                {
+                    //Food Not Available
+                    echo "<div class='error'>Room not found.</div>";
+                }
+    
+           
+            ?>
+
+        
     
     <!-- <div class="row row-cols-1 row-cols-md-3 g-5">
         <div class="col">
