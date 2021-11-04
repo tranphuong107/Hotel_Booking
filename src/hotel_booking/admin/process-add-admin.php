0b2e26ae2b90f12dc56625b305508eb23838197c
Your Name <you@example.com>
2021-11-04 17:39:55 +0700

    add admin

--- src/hotel_booking/admin/process-add-admin.php
+++ src/hotel_booking/admin/process-add-admin.php
@@ -12,7 +12,7 @@
             $pass02 = $_POST['txtPass02'];
             if($pass01 != $pass02){
                 $_SESSION['thongbao'] = 'Mật khẩu không trùng nhau!';
-                header('location:register.php'); 
+                header('location:add-admin.php'); 
             }else{
                 $sql = "SELECT * FROM tb_admins WHERE admin_name ='$admin'";
                 $result = mysqli_query($conn,$sql);
@@ -34,7 +34,7 @@
                         $sql3 = "INSERT INTO tb_admins(admin_name,admin_mobile,admin_email,admin_pass) 
                         VALUES('$admin','$mobile','$email','$pass_hash');";
                         $result3 = mysqli_query($conn,$sql3);
-                        if($result3 >= 1){// nếu đăng ký thành công chuyển sang đăng nhập
+                        if($result3 >= 1){// nếu đăng ký thành công chuyển sang bảng admin
                             header ('location:admin-list.php');
                         }
                     }
