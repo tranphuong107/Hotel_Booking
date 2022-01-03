1febfa281632b3c1ee806fa41b4815c755026fac
ladulee <doh9006@gmail.com>
2022-01-03 09:54:23 +0700

    Sua form them phong, them dich vu

--- src/hotel_booking/admin/service-show.php
+++ src/hotel_booking/admin/service-show.php
@@ -129,29 +129,52 @@
         <div clas = "m-3">
             <a href="service-show.php" id = "close" class = "close" style = "color: #D98E73; text-decoration: none;float: right;font-size:1.5rem;"><i class="far fa-times-circle"></i></a>
         </div>
-        <h1 style = "color: #D98E73;">Thêm dịch vụ</h1>
-        <form action="add-service-process.php" method="POST" enctype="multipart/form-data">
+        <h1 class = "text-center" style = "color: #D98E73;">Thêm dịch vụ</h1>
+        <form action="add-service-process.php" method="POST" class ="pb-5 pt-3 mb-3  mx-auto" style="width:70%" enctype="multipart/form-data">
             <div class = "my-3">
-                Tên dịch vụ <input type="text" name = "ser-name" class = "form-control">
+                <div class="row">
+                    <div class="col-lg-2 pt-2 text-begin">Tên dịch vụ:</div>
+                    <div class="col">
+                        <input type="text" name = "ser-name" class = "form-control">
+                    </div>   
+                </div>
             </div>
-        <div class = "my-3">
-            Số người <input type="number" name = "ser-room-size" min = "1"  class = "form-control">
-            
-        </div>
-        <div class = "my-3">
-            Giá tiền <input type="number"  name = "ser-price"  min = "1"class = "form-control">
-            
-        </div>
         <div  class = "my-3">
-            Mô tả
-            <textarea name ="ser-des"  cols="30" rows="3" class = "form-control" maxlength = "250"></textarea>
+            <div class="row">
+                <div class="col-lg-2 pt-2 text-begin">Mô tả:</div>
+                <div class="col">                   
+                    <textarea name ="ser-des"  cols="30" rows="3" class = "form-control" maxlength = "250"></textarea>
+                </div>
+            </div>
         </div>
         <div class = "my-3">
-            Ảnh <input type="file" name="uploadfile" value=""/>
+            <div class="row">
+                <div class="col-lg-2 pt-2 text-begin">Số người:</div>
+                <div class="col">
+                    <input type="number" name = "ser-room-size" min = "1"  class = "form-control">
+                </div>   
+            </div>
+        </div>
+        <div class = "my-3">
+            <div class="row">
+                <div class="col-lg-2 pt-2 text-begin">Giá tiền:</div>
+                    <div class="col">
+                        <input type="number"  name = "ser-price"  min = "1"class = "form-control">
+                    </div>
+                </div>
+        </div>
+        
+        <div class = "my-3">
+            <div class="row">
+                <div class="col-lg-2 pt-2 text-begin">Ảnh :</div>
+                <div class="col">                   
+                    <input type="file" name="uploadfile" value=""/>
+                </div>
+            </div>
         </div>
         <div class = "mt-3">
             <!-- <a href="" type="submit" name="upload" style = "background: #D98E73; text-decoration: none; color: white" class = "border-5 p-2">Thêm</a> -->
-            <button type="submit" name="upload" style = "background: #D98E73; text-decoration: none; color: white" class = "border-0 p-2">
+            <button type="submit" name="upload" style = "background: #D98E73; text-decoration: none;float:right; color: white" class = "border-0 p-2">
                   Thêm dịch vụ
                 </button>
         </div>
